3e9820ca2dd381a96ebbd88faf3ef80c6865ecd0
khanhdang <dangk13122002@gmail.com>
2021-08-18 07:41:57 +0700

    add landing pages tamplate

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final_exam_app.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
